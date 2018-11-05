3b14ca40a42e298797baa3991605784a3a11979d
= <mikael.ekholm@aalto.fi>
2018-11-05 04:19:22 +0200

    Better version of socket server

--- exercises_1_2/socket_client.py
+++ exercises_1_2/socket_client.py
@@ -1,20 +1,24 @@
-import socket
+import socket, sys
 
 def client_exec():
-    host = socket.gethostname()
+    if len(sys.argv) != 2:
+        print("Please provide host.")
+        return
+    host = sys.argv[1]
     port = 2999
 
-    client_socket = socket.socket()
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     client_socket.connect((host, port))
 
     message = input(" Input address and search term -> ")
 
     while message.lower().strip() != 'close':
-        client_socket.send(message.encode())
+        client_socket.sendall(message.encode())
         data = client_socket.recv(1024).decode()
         print(data)
         message = input(" Input address and search term -> ")
-    
+
     client_socket.close()
 
 if __name__ == '__main__':
--- exercises_1_2/socket_server.py
+++ exercises_1_2/socket_server.py
@@ -1,32 +1,27 @@
 import socket
+import socketserver
 import subprocess
 
-def server_exec():
-    host = socket.gethostname()
-    port = 2999
-
-    server_socket = socket.socket()
-    server_socket.bind((host, port))
-
-    server_socket.listen(2)
-    conn, address = server_socket.accept()
-
-    while True:
-        data = conn.recv(1024).decode()
-        args = data.split(" ")
+class SocketHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip().decode()
+        args = self.data.split(" ")
 
         if len(args) != 2:
-            data = "Incorrect amount of arguments!"
-            conn.send("Incorrect amount of arguments!".encode())
+            self.data = "Incorrect amount of arguments!".encode()
+            self.request.sendall(self.data)
         else:
-            cmd = "home/socket_server/script.sh " + args[0] + " " + args[1]
+            cmd = "/home/socket_server/script.sh " + args[0] + " " + args[1]
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
             (output, err) = p.communicate()
             p_status = p.wait()
-            data = output
-            conn.send(data)
-
-    conn.close()
+            self.data = output
+            self.request.sendall(self.data)
 
 if __name__ == '__main__':
-    server_exec()
+    host = 'localhost'
+    port = 2999
+
+    server = socketserver.TCPServer((host, port), SocketHandler)
+
+    server.serve_forever()
